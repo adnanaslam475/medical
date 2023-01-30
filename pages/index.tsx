2cf63ba4820211ec85565e076f95917ef40289d4
adnanaslam475 <adnanaslam475@gmail.com>
2023-01-31 01:33:23 +0500

    remove muistyles....3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 // import '../styles/base.scss'
 // import { Inter } from '@next/font/google'
 import Questionarie from '../src/components/Questionarie';
