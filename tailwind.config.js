342bf6603e92619585741bcd773f824f59b6288f
adnanaslam475 <adnanaslam475@gmail.com>
2023-01-31 10:51:59 +0500

    remove muistyles....7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
+//   darkMode: "class",
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
