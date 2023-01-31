b1caded76637aa7554102e1e72c2067554a2a96e
adnanaslam475 <adnanaslam475@gmail.com>
2023-01-31 11:08:15 +0500

    remove muistyles....9

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/base.scss";
 import type { AppProps } from "next/app";
 
 import Router from "next/router";
+import { useEffect } from "react";
 
 const routeChange = () => {
   // Temporary fix to avoid flash of unstyled content
@@ -23,5 +24,13 @@ Router.events.on("routeChangeComplete", routeChange);
 Router.events.on("routeChangeStart", routeChange);
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // import("tw-elements");
+    const use = async () => {
+      (await import("tw-elements")).default as any;
+    };
+    use();
+  }, []);
+
   return <Component {...pageProps} />;
 }
